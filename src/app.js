dbbd9b6a79c01ec9f3c8c6e7d8ec480addd40db3
Saqib <Saqibamin72@gmail.com>
2024-12-17 14:36:10 +0530

    routing testing

--- src/app.js
+++ src/app.js
@@ -1,18 +1,27 @@
-const express=require('express');
-const app=express();
-app.use((req,res)=>{
-    res.send("server dashbaord");
-})
-app.use("/hello",(req,res)=>{
-    res.send("hello hello");
-})
-app.use("/wah",(req,res)=>{
-    res.send("good to go");
-})
+const express = require('express');
+const app = express();
+app.get("/user", (req, res) => {
+    res.send({ firstname: "saqib", lastname: "amin" });
 
+});
+app.get("/user/:usedId/:name/:password", (req, res) => {
+    console.log(req.params);
     
+    res.send({ firstname: "saqib", lastname: "amin" });
 
-app.listen(7777,() => {
+});
+app.post("/user", (req, res) => {
+    res.send("data successfully posted on DB")
+})
+app.delete("/user",(req, res)=>{
+    res.send("deleted successfully");
+});
+app.get("/ab*cd",(req,res)=>{
+    res.send("abc exact");
+});
+
+
+app.listen(7777, () => {
     console.log("server is successfull listening on port 7777");
-    
+
 })
