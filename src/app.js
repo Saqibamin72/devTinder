154bf08200a3cc99f51ac63c3f3b01eaf69c90ab
Saqib <Saqibamin72@gmail.com>
2024-12-19 10:48:56 +0530

    Dynamic routing

--- src/app.js
+++ src/app.js
@@ -1,27 +1,12 @@
 const express = require('express');
 const app = express();
-app.get("/user", (req, res) => {
-    res.send({ firstname: "saqib", lastname: "amin" });
-
-});
-app.get("/user/:usedId/:name/:password", (req, res) => {
+app.get("/user/:userId/:name/:password",(req, res)=>{
     console.log(req.params);
-    
-    res.send({ firstname: "saqib", lastname: "amin" });
-
-});
-app.post("/user", (req, res) => {
-    res.send("data successfully posted on DB")
-})
-app.delete("/user",(req, res)=>{
-    res.send("deleted successfully");
-});
-app.get("/ab*cd",(req,res)=>{
-    res.send("abc exact");
+    res.send({firstname:"saqib",lastname:"dar"});
 });
 
 
 app.listen(7777, () => {
     console.log("server is successfull listening on port 7777");
 
-})
+}) 
