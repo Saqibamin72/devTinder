3b06ab43b597c5247fbeb5745dfbd2593d331d1f
Saqib <Saqibamin72@gmail.com>
2024-12-19 11:06:17 +0530

    Multiple routes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const app = express();
-app.get("/user/:userId/:name/:password",(req, res)=>{
-    console.log(req.params);
-    res.send({firstname:"saqib",lastname:"dar"});
-});
+app.use("/user", (req,res, next)=>{
+    console.log("handing route user1");
+    // res.send("response 1")
+    next();
+    
+},
+(req,res,next)=>{
+    console.log("hanlding route 2");
+    // res.send("response 2")
+    next();
+
+},
+(req,res)=>{
+    console.log("hanlding route 3");
+    res.send("response 3")
+}
+)
+
 
 
 app.listen(7777, () => {
