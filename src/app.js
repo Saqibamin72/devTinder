138a72bd77dfa00a6847a73830dccfe7bbd45700
Saqib <Saqibamin72@gmail.com>
2024-12-21 16:57:42 +0530

    Post signUp Api

--- src/app.js
+++ src/app.js
@@ -1,6 +1,30 @@
 const express=require("express");
-const connectDB=require('./config/database');
+const connectDB=require("./config/database");
 const app=express();
+const User=require("./models/user");
+app.post("/signUp", async(req,res)=>{
+   
+    const user=new User({
+        firstName:"irfan",
+        lastName:"sheikh",
+        emailId:"Sirfn@gmail.com",
+        password:"aifal@123",
+        _id: "8099821937983773",
+    })
+    try{ 
+
+        await user.save();
+        res.send("User added successfully");
+    }
+    catch(err){
+        res.status(400).send("User not saved:"+err.message);
+    }
+   
+
+});
+
+
+
 
 connectDB()
 .then(()=>{
